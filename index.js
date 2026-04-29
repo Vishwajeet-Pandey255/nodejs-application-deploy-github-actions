48f79fa1a93e2eb21290e9abe9c2025b291db8a7
Vishwajeet Pandey <pandeyvishwajeet61@gmail.com>
2026-04-29 12:17:48 +0530

    Hello Prince

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8080;
 
 app.get('/', (req, res) => {
-    return res.json({msg: 'Hello from the server V4!'});
+    return res.json({msg: 'Hello from the server from prince!'});
 })
 
 
